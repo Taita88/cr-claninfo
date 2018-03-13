cc536cc1d4657a7d206c35d9e0155c80d9bc121d
Tibor Bucsuhazy <tibor.bucsuhazy@atos.net>
2018-03-13 12:39:31 +0100

    added jQuery + datatables, implementing FE visual

--- index.php
+++ index.php
@@ -1,4 +1,44 @@
 <html>
+<head>
+    <link rel="stylesheet" type="text/css" href="js/Datatables-1.10.16/datatables.min.css" />
+    <script type="text/javascript" src="js/jquery-1.12.4.js"></script>
+    <script type="text/javascript" src="js/Datatables-1.10.16/datatables.min.js"></script>    
+    <script type="text/javascript" src="js/jquery.dataTables.min.js"></script> 
+    
+    <style type="text/css">
+        div.container {
+            width: 40%;  
+        }
+        
+        #donation {
+            display: none;
+        }              
+        
+        .btn{
+            height: 28px;
+            text-align: center;
+            background-color: #F8F8F8;
+            border-radius: 3px;
+            border: 2px solid #E8E8E8;
+        }       
+    </style>
+</head>
+<body onLoad="defaultTableShow()">
+	
+	<script type="text/javascript">			 	
+     	$(document).ready(function() {
+    	    $('#donation').DataTable({
+    	    	"paging":   false,
+    	    	"columnDefs": [
+    	    	    { "width": "100px",  "targets": 0}
+    	    	  ],
+    	    	"fixedColumns": true,
+    	    	"info": false,
+    	    	"searching": false
+    	    });
+    	} );
+    
+     </script>
 
   
 <?php
@@ -15,57 +55,105 @@ header('Content-Type: text/html; charset=utf-8');
 
 ?>
 
-  <body>
-	<table border=1>
+
+    	<button id="donation_btn" class="btn">Donation</button>
+		<button id="crown_btn" class="btn">Crown</button>
+	
     
     <?php
-    
-    // $log = new Logger('name');
-    // $log->pushHandler(new StreamHandler('cr.log', Logger::DEBUG));
-    // $log->debug("Logging");
-    
-    
-    // var_dump($cycle_dao->getAllCycleWithMembers());
-    
     $clan_info_table_data = new ClanInfoTableData();
     
+    //donation table
+    echo "<div class=\"container\" >";
+    echo "<table id=\"donation\" class=\"display\" cellspacing=\"0\" width=\"100%\">";   
+    echo "<thead>";
     echo "<tr>";
-    echo "<td></td>";
+    echo "<th></th>";
     
-    foreach ($clan_info_table_data->getCycles() as $cycle) 
-    {
-        echo "<td>" . $cycle->getYear() . "/" . $cycle->getWeekOfYear() . "</td>";
-    }
+    foreach ($clan_info_table_data->getCycles() as $cycle) {
+        echo "<th>" . $cycle->getYear() . "/" . $cycle->getWeekOfYear() . "</th>";
+        echo "<th>" . $cycle->getYear() . "/" . $cycle->getWeekOfYear() . "</th>";
+    }       
     
     echo "</tr>";
-        
-    $i = 1;
-    foreach ($clan_info_table_data->getMembers() as $member)
-    {
+    echo "</thead>";
+    echo "<tbody>";
+    
+    foreach ($clan_info_table_data->getMembers() as $member) {
         
         echo "<tr>";
-        echo "<td>" . $i++ . " " . $member->getName() . "</td>";
-            
-        foreach ($member->getActionPerCycle() as $action_per_cycle)
-        {
+        echo "<td>" . $member->getName() . "</td>";
+        
+        foreach ($member->getActionPerCycle() as $action_per_cycle) {
             echo "<td>" . $action_per_cycle->getDonation() . "</td>";
+            echo "<td>" . $action_per_cycle->getCrown() . "</td>";
         }
         
         echo "</tr>";
     }
     
     
+    echo "</tbody>";
+    echo "</table>";
+    echo "</div>";
     
-    // var_dump(new ClanInfoTableData());
     
-    ?>
-    </table>
     
-    <?php 
-//     $json = json_encode((array)($clan_info_table_data->toAssocArray()), JSON_PRETTY_PRINT);
-//         echo "<br>" . $json;
     ?>
 
+    
+    <?php
+    // $json = json_encode((array)($clan_info_table_data->toAssocArray()), JSON_PRETTY_PRINT);
+    // echo "<br>" . $json;
+    ?>
+    
+   
+	<script type="text/javascript">	
+
+	function defaultTableShow(){
+		
+		
+    	document.getElementById('donation').style.display = 'block'; 
+    	document.getElementById("donation_btn").style.borderColor = 'blue';
+    	document.getElementById("crown_btn").style.borderColor = '';
+
+    	hideColumn("CROWN");    	
+	}	
+
+	$('#donation_btn').click(function(e) {
+		hideColumn("CROWN");
+		
+// 		e.preventDefault();
+		document.getElementById("donation_btn").style.borderColor = 'blue';
+		document.getElementById("crown_btn").style.borderColor = '';			
+	});
+	
+	$('#crown_btn').click(function(e) {
+		hideColumn("DONATION");
+		
+//     	e.preventDefault();
+		document.getElementById("crown_btn").style.borderColor = 'blue'; 
+		document.getElementById("donation_btn").style.borderColor = '' ; 			
+	});	
+
+	function hideColumn(columnType){
+		var table = $('#donation').DataTable();
+    	var colCount = table.columns().header().length;
+
+    	for(var i = 1; i < colCount; i++){
+    		if(columnType === "DONATION"){
+        		(i % 2 != 1) ? table.column(i).visible(true) : table.column(i).visible(false);
+    		}
+
+    		if(columnType === "CROWN"){
+    			(i % 2 == 1) ? table.column(i).visible(true) : table.column(i).visible(false);
+    		}
+    	}
+    	table.columns.adjust().draw( true );
+
+	}		
+		 
+	</script>
 
 </body>
 </html>
